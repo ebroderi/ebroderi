d48b4c126ad56d29585bef0f8301bb86a958d1b3
Emily Broderick <ebroderi@tcd.ie>
2019-10-03 15:01:00 +0100

    Added lowestCommonAncestor code

--- LCA2/src/lca.java
+++ LCA2/src/lca.java
@@ -25,10 +25,28 @@ public class lca<Key extends Comparable<Key>, Value>
 	}
 
 	
-	public Key LCA() {
-		return null;
+	public Key lowestCommonAncestor(Node node, Key key1, Key key2) {
+		if (node == null)
+            return null;
+		if (node.key == key1) {
+			return node.key;
+		}
+		if (node.key == key2) {
+			return node.key;
+		}
+		int cmp1 = node.key.compareTo(key1);
+		int cmp2 = node.key.compareTo(key2);
+		
+        if (cmp1 >= 0 && cmp2 >= 0)
+            return lowestCommonAncestor(node.left, key1, key2);
+  
+        if (cmp1 <= 0 && cmp2 <= 0)
+            return lowestCommonAncestor(node.right, key1, key2);
+  
+        return node.key;
 	}
 }
 
 //Code from BST assignment in second year
 //Removed unnecessary functions
+//Consulted code from geeksforgeeks.org
