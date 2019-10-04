7d066b0cc68231006c81ef660987fbdf2106ee0d
Emily Broderick <ebroderi@tcd.ie>
2019-10-04 18:06:28 +0100

    Added test for put and defined other tests. Replaced some code

--- LCA2/src/lca.java
+++ LCA2/src/lca.java
@@ -7,7 +7,10 @@ import java.util.NoSuchElementException;
 
 public class lca<Key extends Comparable<Key>, Value> 
 {
-	
+	private Node root;             
+	private String stringOutput = "";
+	private String multiLineOutput = "";
+	private String prefix = "";
 	private class Node 
 	{
 		private Key key;          
@@ -24,10 +27,139 @@ public class lca<Key extends Comparable<Key>, Value>
 		}
 	}
 
-	
+
+	public void put(Key key, Value val) 
+	{
+		if (val == null) 
+		{ 
+			delete(key); 
+			return; 
+		}
+		root = put(root, key, val);
+	}
+
+	private Node put(Node x, Key keyToPlaceValIn, Value newValue) 
+	{
+		if (x == null) 
+			return new Node(keyToPlaceValIn, newValue, 1);
+
+		int cmp = keyToPlaceValIn.compareTo(x.key);
+
+		if (cmp < 0) 
+			x.left  = put(x.left,  keyToPlaceValIn, newValue);
+
+		else if (cmp > 0) 
+			x.right = put(x.right, keyToPlaceValIn, newValue);
+
+		else             
+			x.value   = newValue;
+
+		x.N = 1 + size(x.left) + size(x.right);
+
+		return x;
+	}
+
+	public int size() 
+	{ 
+		return size(root); 
+	}
+
+	private int size(Node x) 
+	{
+		if (x == null) 
+			return 0;
+
+		else 
+			return x.N;  
+	}
+
+
+	private Node max(Node x)
+	{
+		if (x.right == null) 
+			return x;
+
+		return max(x.right);
+	}
+
+	public void delete(Key key)
+	{ 
+		root = delete(root, key); 
+	}
+
+	private Node delete(Node x, Key key)
+	{
+		if (x == null) 
+			return null;
+
+		int compare = key.compareTo(x.key);
+
+		if (compare < 0) x.left = delete(x.left, key);
+
+		else if (compare > 0) x.right = delete(x.right, key);
+
+		else
+		{
+			if (x.right == null) 
+				return x.left;
+
+			if (x.left == null) 
+				return x.right;
+
+			Node t = x;
+			x = max(t.left); 
+			x.left = deleteMax(t.left);
+			x.right = t.right;
+		}
+
+		x.N = size(x.left) + size(x.right) + 1;
+		return x;
+	}
+
+	private Node deleteMax(Node x)
+	{
+		if (x.right == null) 
+			return x.left;
+
+		x.right = deleteMax(x.right);
+
+		x.N = size(x.right) + size(x.left) + 1;
+
+		return x;
+	}
+
+	public String printKeysInOrder()
+	{
+		stringOutput = "";
+
+		if (this.root == null) 
+			return "()";
+
+		else
+			return "(" + printKeysInOrder(this.root) + ")";
+	}
+
+	private String printKeysInOrder(Node x)
+	{
+		if (x == null) 
+			return "";
+
+		stringOutput += "(";
+		printKeysInOrder(x.left);
+		stringOutput += ")";
+
+		stringOutput += (x.key);
+
+		stringOutput += "(";
+		printKeysInOrder(x.right);
+		stringOutput += ")";
+
+		return stringOutput;
+	}
+
 	public Key lowestCommonAncestor(Node node, Key key1, Key key2) {
 		if (node == null)
-            return null;
+			return null;
 		if (node.key == key1) {
 			return node.key;
 		}
@@ -36,14 +168,14 @@ public class lca<Key extends Comparable<Key>, Value>
 		}
 		int cmp1 = node.key.compareTo(key1);
 		int cmp2 = node.key.compareTo(key2);
-		
-        if (cmp1 >= 0 && cmp2 >= 0)
-            return lowestCommonAncestor(node.left, key1, key2);
-  
-        if (cmp1 <= 0 && cmp2 <= 0)
-            return lowestCommonAncestor(node.right, key1, key2);
-  
-        return node.key;
+
+		if (cmp1 >= 0 && cmp2 >= 0)
+			return lowestCommonAncestor(node.left, key1, key2);
+
+		if (cmp1 <= 0 && cmp2 <= 0)
+			return lowestCommonAncestor(node.right, key1, key2);
+
+		return node.key;
 	}
 }
 
--- LCA2/src/lcaTest.java
+++ LCA2/src/lcaTest.java
@@ -5,8 +5,36 @@ import org.junit.jupiter.api.Test;
 class lcaTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testLowestCommonAncestor() {
+
+	}
+
+	@Test
+	public void testPut() {
+		lca<Integer, Integer> lca = new lca<Integer, Integer>();
+
+		lca.put(5, null);
+		lca.put(7, 7);
+		lca.put(8, 8);     
+		lca.put(3, 3);       
+		lca.put(1, 1);      
+		lca.put(2, 2);      
+		lca.put(6, 6);       
+		lca.put(4, 4);       
+		lca.put(5, 5); 
+		lca.put(5, 5); 
+
+		assertEquals("(((()1(()2()))3((()4(()5()))6()))7(()8()))", lca.printKeysInOrder());
+	}
+//From last year's assignment 
+	@Test
+	public void testSize() {
+
+	}
+
+	@Test	
+	public void testDelete() {
+
 	}
 
 }
