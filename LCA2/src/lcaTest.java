7d314162109f10468858fab7ca5f3af77584b712
Emily Broderick <ebroderi@tcd.ie>
2019-10-08 12:24:19 +0100

    Checked tests and installed newer version of Eclipse

--- LCA2/src/lcaTest.java
+++ LCA2/src/lcaTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class lcaTest {
 
