871989aa184455a0ff21483897f2172200d9fbf9
Emily Broderick <ebroderi@tcd.ie>
2019-10-10 13:58:22 +0100

    Stated some functions in DAG class

--- LCA2/src/DAG.java
+++ LCA2/src/DAG.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
 
 public class DAG {
-
+	private int V;
+	private int E;
+	private ArrayList<Integer>[] adj; 
+	
+	public int V() {
+		return V;
+	}
+	
+	public int E() {
+	return E;	
+	}
+	
+	public void addEdge(int v, int w)
+	{
+		
+	}
+	
+	public Iterable<Integer> adj(int v){
+		return adj[v];
+	}
+	
+	public DAG reverse() {
+		return null;
+	}
 }
+//Functions from https://algs4.cs.princeton.edu/42digraph/ - last year's course book 
