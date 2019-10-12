e684f0849db6ab5440acca276ad242d746e73bfe
Emily Broderick <ebroderi@tcd.ie>
2019-10-12 13:27:20 +0100

    Added Sedgewick and Wayne's Digraph class

--- LCA2/src/DAG.java
+++ LCA2/src/DAG.java
@@ -1,9 +1,22 @@
 import java.util.ArrayList;
 
 public class DAG {
-	private int V;
+	private final int V;
 	private int E;
 	private ArrayList<Integer>[] adj; 
+	private int[] indegree;
+	
+	public DAG(int V){
+		if(V<0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+		this.V=V;
+		this.E=0;
+		indegree=new int[V];
+		adj=(ArrayList<Integer>[]) new ArrayList[V];
+		for(int v=0; v<V; v++)
+		{
+			adj[v]= new ArrayList<Integer>();
+		}
+	}
 	
 	public int V() {
 		return V;
@@ -13,17 +26,44 @@ public class DAG {
 	return E;	
 	}
 	
+	private void validateVertex(int v) {
+		if(v<0||v>=V) {
+			throw new IllegalArgumentException("vertex"+v+"is not between 0 and "+(V-1));
+		}
+	}
+	
 	public void addEdge(int v, int w)
 	{
-		
+		validateVertex(v);
+		validateVertex(w);
+		adj[v].add(w);
+		indegree[w]++;
+		E++;
 	}
 	
 	public Iterable<Integer> adj(int v){
+		validateVertex(v);
 		return adj[v];
 	}
 	
+	public int outdegree(int v) {
+		validateVertex(v);
+		return adj[v].size();
+	}
+	
+	public int indegree(int v) {
+		validateVertex(v);
+		return indegree[v];
+	}
+	
 	public DAG reverse() {
-		return null;
+		DAG reverse=new DAG(V);
+		for(int v=0; v<V; v++) {
+			for(int w:adj(v)) {
+				reverse.addEdge(w, v);
+			}
+		}
+		return reverse;
 	}
 }
-//Functions from https://algs4.cs.princeton.edu/42digraph/ - last year's course book 
+//Using Sedgewick and Wayne's Digraph class 
