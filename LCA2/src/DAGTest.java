682123fc426cbec7869f104ec89af6bfa082482a
Emily Broderick <ebroderi@tcd.ie>
2019-10-17 11:38:04 +0100

    Created emptyGraphTest()

--- LCA2/src/DAGTest.java
+++ LCA2/src/DAGTest.java
@@ -78,38 +78,45 @@ class DAGTest {
 		 
 	}
 	
+	@Test
 	public void noCommonAncestorsTest() {
-		DAG lca=new DAG(8);
-		lca.addEdge(0, 1);
-		lca.addEdge(0, 2);
-		lca.addEdge(1, 3);
-		lca.addEdge(3, 4);
-		lca.addEdge(3, 5);
-		lca.addEdge(4, 6);
-		lca.addEdge(5, 6);
+		DAG noCommonAncestors=new DAG(8);
+		noCommonAncestors.addEdge(0, 1);
+		noCommonAncestors.addEdge(0, 2);
+		noCommonAncestors.addEdge(1, 3);
+		noCommonAncestors.addEdge(3, 4);
+		noCommonAncestors.addEdge(3, 5);
+		noCommonAncestors.addEdge(4, 6);
+		noCommonAncestors.addEdge(5, 6);
 		
-		assertEquals("LCA for 1 and 2", 0, lca.findLCA(1, 2));
-		assertEquals("LCA for 3 and 4", 1, lca.findLCA(3, 4));
-		assertEquals("LCA when node doesn't exist", -1, lca.findLCA(5, 7));
+		assertEquals("LCA for 1 and 2", 0, noCommonAncestors.findLCA(1, 2));
+		assertEquals("LCA for 3 and 4", 3, noCommonAncestors.findLCA(3, 4));
+		assertEquals("LCA when node doesn't exist", -1, noCommonAncestors.findLCA(5, 7));
 	}
 	
+	@Test
 	public void cyclicGraphTest() {
-		DAG lca=new DAG(8);
-		lca.addEdge(0, 1);
-		lca.addEdge(0, 2);
-		lca.addEdge(2, 3);
-		lca.addEdge(3, 4);
-		lca.addEdge(4, 2);
-		lca.addEdge(4, 5);
-		lca.addEdge(5, 6);
-		lca.addEdge(6, 7);
+		DAG cyclicGraph=new DAG(8);
+		cyclicGraph.addEdge(0, 1);
+		cyclicGraph.addEdge(0, 2);
+		cyclicGraph.addEdge(2, 3);
+		cyclicGraph.addEdge(3, 4);
+		cyclicGraph.addEdge(4, 2);
+		cyclicGraph.addEdge(4, 5);
+		cyclicGraph.addEdge(5, 6);
+		cyclicGraph.addEdge(6, 7);
 		
-		assertEquals("Cyclic graph so answer should be -1", -1, lca.findLCA(1, 2));
-		assertEquals("Cyclic graph so answer should be -1", -1, lca.findLCA(2, 3));
-		assertEquals("Cyclic graph so answer should be -1", -1, lca.findLCA(4, 5));
-		assertEquals("Cyclic graph so answer should be -1", -1, lca.findLCA(6, 7));
+		assertEquals("Cyclic graph so answer should be -1", -1, cyclicGraph.findLCA(1, 2));
+		assertEquals("Cyclic graph so answer should be -1", -1, cyclicGraph.findLCA(2, 3));
+		assertEquals("Cyclic graph so answer should be -1", -1, cyclicGraph.findLCA(4, 5));
+		assertEquals("Cyclic graph so answer should be -1", -1, cyclicGraph.findLCA(6, 7));
 	}
 	
-	
+	@Test
+	public void emptyGraphTest() {
+		DAG empty=new DAG(8);
+		
+		assertEquals("LCA should be -1", -1, empty.findLCA(0, 1));
+	}
 
 }
